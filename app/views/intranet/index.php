cf054f1213fd05287ae69b9d7afb773a781fadae
anibalyucra20 <anibalyucrac@gmail.com>
2025-07-11 20:31:13 -0500

    Update index.php

--- app/views/intranet/index.php
+++ app/views/intranet/index.php
@@ -1,4 +1,5 @@
-<?php require __DIR__ . '/../layouts/header.php'; ?>
+<!--<?php require __DIR__ . '/../layouts/header.php'; ?>-->
+<?php require '../layouts/header.php'; ?>
 <?php if (!empty($_SESSION['flash_error'])): ?>
   <div class="alert alert-danger alert-dismissible">
     <?= $_SESSION['flash_error'] ?>
@@ -23,4 +24,5 @@
   <?php endforeach; ?>
 </div>
 
-<?php require __DIR__ . '/../layouts/footer.php'; ?>
+<!--<?php require __DIR__ . '/../layouts/footer.php'; ?>-->
+<?php require '../layouts/footer.php'; ?>
