ab6b1d8756474798fee5634afab4b41a29c8f5b0
anibalyucra20 <anibalyucrac@gmail.com>
2025-07-11 20:25:54 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,11 +17,17 @@ session_set_cookie_params([
 //require_once __DIR__ . '/../config/config.php';
 require_once '../config/config.php';
 // Núcleo MVC
-require_once __DIR__ . '/../core/Model.php';
+/*require_once __DIR__ . '/../core/Model.php';
 require_once __DIR__ . '/../core/App.php';
 require_once __DIR__ . '/../core/Auth.php';
 require_once __DIR__ . '/../core/Controller.php';
-require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/../core/Router.php';*/
+
+require_once '../core/Model.php';
+require_once '../core/App.php';
+require_once '../core/Auth.php';
+require_once '../core/Controller.php';
+require_once '../core/Router.php';
 
 // Arranca la aplicación
 $app = new \Core\App();
