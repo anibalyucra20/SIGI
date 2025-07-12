27fea5ba014cde2864d4ecceaaf5f10ee424e5aa
anibalyucra20 <anibalyucrac@gmail.com>
2025-07-12 07:59:59 -0500

    Update login.php

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,4 +1,4 @@
-<?php require __DIR__ . '/../layouts/header.php'; ?>
+<?php require '../layouts/header.php'; ?>
 <style>
     body {
         margin: 0;
@@ -92,4 +92,4 @@
     </form>
     <a href="#">¿Olvidaste tu contraseña?</a>
   </div>
-<?php require __DIR__ . '/../layouts/footer.php'; ?>
+<?php require '../layouts/footer.php'; ?>
