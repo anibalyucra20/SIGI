4dd73bce8358c6bc5c193c1637beef17f87c34cb
anibal yucra <105953269+anibalyucra20@users.noreply.github.com>
2026-07-15 08:57:47 -0500

    Update login.php

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -118,7 +118,7 @@
                 <hr class="mt-2 mb-3">
                 <div class="text-center">
                     <!-- Reemplaza "NUMERO_AQUI" con el número incluyendo el código de país (Ej: 51999888777 para Perú) -->
-                    <a href="https://wa.me/+51943524805?text=Hola,%20necesito%20soporte%20con%20el%20sistema%20SIGI" target="_blank" class="btn btn-whatsapp btn-block">
+                    <a href="https://wa.me/+51986067472?text=Hola,%20necesito%20soporte%20con%20el%20sistema%20SIGI" target="_blank" class="btn btn-whatsapp btn-block">
                         <i class="fab fa-whatsapp mr-1"></i> Contactar a Soporte
                     </a>
                 </div>
