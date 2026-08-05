efe65b898bbdd29a717807668823b5820885b36b
anibal yucra <105953269+anibalyucra20@users.noreply.github.com>
2026-08-05 11:40:59 -0500

    Update pdf_acta_recuperacion.php

--- app/views/academico/calificaciones/pdf_acta_recuperacion.php
+++ app/views/academico/calificaciones/pdf_acta_recuperacion.php
@@ -134,7 +134,7 @@ if (!$permitido) {
     </thead>
     <tbody>
         <?php 
-        $contador = 0;
+        $contador = 1;
         foreach ($estudiantes as $idx => $est):
             $id_detalle = $est['id_detalle_matricula'];
 
@@ -147,7 +147,7 @@ if (!$permitido) {
                 }
         ?>
                 <tr>
-                    <td style="text-align:center; width:10%;"><?= ($contador + 1) ?></td>
+                    <td style="text-align:center; width:10%;"><?= ($contador) ?></td>
                     <td style="text-align:center; width:15%;"><?= $est['dni'] ?? '' ?></td>
                     <td class="nombres" style="width:40%;"><?= $est['apellidos_nombres'] ?? '' ?></td>
                     <td style="text-align:center; width:13%;" class="<?= $clase; ?>"><?= $promedio_final ?? '' ?></td>
